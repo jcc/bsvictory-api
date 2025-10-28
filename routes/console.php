0a4a1bd41659878fdac994e3029e51f8e5849fb7
Jiajian Chan <changejian@gmail.com>
2025-10-28 14:16:33 +0800

    Optimize

--- routes/console.php
+++ routes/console.php
@@ -3,8 +3,17 @@
 use App\Console\Commands\UpdateBidding;
 use App\Console\Commands\UpdateSeconds;
 use App\Console\Commands\UpdateYesterday;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command(UpdateYesterday::class)->dailyAt('09:26');
-Schedule::command(UpdateBidding::class)->everyFiveSeconds()->between('09:26', '09:30');
-Schedule::command(UpdateSeconds::class)->everyFiveSeconds()->between('09:30', '11:30')->between('13:00', '15:00');
+Schedule::command(UpdateYesterday::class)
+    ->dailyAt('09:26');
+
+Schedule::command(UpdateBidding::class)
+    ->everyFiveSeconds()
+    ->between('09:26', '09:30');
+
+Schedule::command(UpdateSeconds::class)
+    ->everyFiveSeconds()
+    ->between('09:30', '11:30')
+    ->between('13:00', '15:00');
