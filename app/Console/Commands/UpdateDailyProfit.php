7022939e37708a74ed6d8e7e2a90911179c54f2e
Jiajian Chan <changejian@gmail.com>
2025-11-07 14:27:57 +0800

    feat: 统计上一日收益

--- app/Console/Commands/UpdateDailyProfit.php
+++ app/Console/Commands/UpdateDailyProfit.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Models\DailyProfit;
 use App\Models\Stock;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 
 class UpdateDailyProfit extends Command
@@ -27,14 +28,50 @@ class UpdateDailyProfit extends Command
      */
     public function handle()
     {
-        // 统计每日平均收益
-        $dailyAverages = Stock::selectRaw('DATE(created_at) as date, AVG(actual_profit) as profit, COUNT(id) as stock_count, SUM(CASE WHEN actual_profit > 0 THEN 1 ELSE 0 END) as profit_stock_count, SUM(CASE WHEN actual_profit < 0 THEN 1 ELSE 0 END) as loss_stock_count, SUM(actual_profit) as total_profit')
-                       ->groupBy('date')
-                       ->orderBy('date', 'asc')
-                       ->get();
+        $prevDay = $this->getPreviousTradingDay();
 
-        DailyProfit::query()->upsert($dailyAverages->toArray(), ['date'], ['profit', 'total_profit', 'stock_count', 'profit_stock_count', 'loss_stock_count']);
+        // 统计每日平均收益
+        $dailyAverages = Stock::selectRaw('AVG(actual_profit) as profit, COUNT(id) as stock_count, SUM(CASE WHEN actual_profit > 0 THEN 1 ELSE 0 END) as profit_stock_count, SUM(CASE WHEN actual_profit < 0 THEN 1 ELSE 0 END) as loss_stock_count, SUM(actual_profit) as total_profit')
+                       ->whereDate('created_at', $prevDay)
+                       ->first();
+
+        $record = DailyProfit::whereDate('date', $prevDay)->first();
+
+        if ($dailyAverages->stock_count > 0) {
+            if (!$record) {
+                DailyProfit::create([
+                    'date' => $prevDay,
+                    'profit' => $dailyAverages->profit,
+                    'total_profit' => $dailyAverages->total_profit,
+                    'stock_count' => $dailyAverages->stock_count,
+                    'profit_stock_count' => $dailyAverages->profit_stock_count,
+                    'loss_stock_count' => $dailyAverages->loss_stock_count,
+                ]);
+            } else {
+                $record->update([
+                    'profit' => $dailyAverages->profit,
+                    'total_profit' => $dailyAverages->total_profit,
+                    'stock_count' => $dailyAverages->stock_count,
+                    'profit_stock_count' => $dailyAverages->profit_stock_count,
+                    'loss_stock_count' => $dailyAverages->loss_stock_count,
+                ]);
+            }
+        }
 
         return 0;
+    }
+
+    /**
+     * 获取上一个交易日日期
+     */
+    protected function getPreviousTradingDay($date = null)
+    {
+        $date = $date ? Carbon::parse($date) : Carbon::now();
+
+        do {
+            $date = $date->subDay();
+        } while ($date->isWeekend());
+
+        return $date->format('Y-m-d');
     }
 }
--- routes/console.php
+++ routes/console.php
@@ -1,13 +1,10 @@
 <?php
 
 use App\Console\Commands\UpdateBidding;
+use App\Console\Commands\UpdateDailyProfit;
 use App\Console\Commands\UpdateSeconds;
 use App\Console\Commands\UpdateYesterday;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-
-Schedule::command(UpdateYesterday::class)
-    ->dailyAt('09:26');
 
 Schedule::command(UpdateBidding::class)
     ->everyFiveSeconds()
@@ -16,3 +13,6 @@ Schedule::command(UpdateBidding::class)
 Schedule::command(UpdateSeconds::class)
     ->everyFiveSeconds()
     ->between('09:30', '15:00');
+
+Schedule::command(UpdateYesterday::class)->dailyAt('09:26');
+Schedule::command(UpdateDailyProfit::class)->dailyAt('09:28');
